bfbb92190f65f3178d269d47e928a228a9f5ac9f
Mehdi Charef <me-charef@hotmail.fr>
2024-10-04 19:49:53 +0200

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import footerLogo from '../../assets/logo_footer.png';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <p>footer</p>
-      <img src={footerLogo} alt='logo Kasa footer' />
+    <div className='footer'>
+      <img src={footerLogo} alt='logo Kasa footer' className='footer__logo' />
+      <p className='footer__text'>© 2020 Kasa. All rights reserved</p>
     </div>
   );
 };
