3b3361c6f1aee270aada9c9bb05823550c7cfbc8
Mehdi Charef <me-charef@hotmail.fr>
2024-10-06 23:58:57 +0200

    not found page done

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>
-      {' '}
-      <p>not found</p>
+    <div className='notFound__container'>
+      <p className='notFound__404'>404</p>
+      <p className='notFound__message'>
+        Oups! La page que{' '}
+        <span className='mobile-line-break'>
+          <br />{' '}
+        </span>{' '}
+        vous demandez n'existe pas.
+      </p>
+      <Link to='/' className='notFound__link'>
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 };
