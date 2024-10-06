85a1d742099f434266f40ea0a9f534c02c747243
Mehdi Charef <me-charef@hotmail.fr>
2024-10-06 23:06:45 +0200

    added animations for collapse

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,3 +1,28 @@
+// import React, { useState } from 'react';
+// import './Collapse.css';
+
+// const Collapse = ({ title, content }) => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const handleCollapse = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div className='collapse'>
+//       <button className='collapse__button' onClick={handleCollapse}>
+//         <p className='collapse__title'>{title}</p>
+//         <i
+//           className={`fa-solid fa-chevron-up fa-xl collapse__icon ${
+//             isOpen ? 'collapse__icon--down' : 'collapse__icon--up'
+//           }`}
+//         ></i>
+//       </button>
+//       {isOpen && <p className='collapse__content'>{content}</p>}
+//     </div>
+//   );
+// };
+
+// export default Collapse;
 import React, { useState } from 'react';
 import './Collapse.css';
 
@@ -6,18 +31,26 @@ const Collapse = ({ title, content }) => {
   const handleCollapse = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <div className='collapse'>
       <button className='collapse__button' onClick={handleCollapse}>
         <p className='collapse__title'>{title}</p>
-        {isOpen ? (
-          <i className='fa-solid fa-chevron-down fa-xl collapse__icon'></i>
-        ) : (
-          <i className='fa-solid fa-chevron-up fa-xl collapse__icon'></i>
-        )}
+        <i
+          className={`fa-solid fa-chevron-up fa-xl collapse__icon ${
+            isOpen ? 'collapse__icon--down' : 'collapse__icon--up'
+          }`}
+        ></i>
       </button>
-      {isOpen ? <p className='collapse__content'>{content}</p> : null}
+      <div
+        className={`collapse__content ${
+          isOpen ? 'collapse__content--open' : ''
+        }`}
+      >
+        {content}
+      </div>
     </div>
   );
 };
+
 export default Collapse;
