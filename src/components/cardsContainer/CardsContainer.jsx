914113a0358a4bea7421f4d7ca931c677ab21a12
Mehdi Charef <me-charef@hotmail.fr>
2024-10-07 00:38:44 +0200

    filtered selected apartment

--- src/components/cardsContainer/CardsContainer.jsx
+++ src/components/cardsContainer/CardsContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './CardsContainer.css';
 import datas from '../../assets/housingOffers.json';
 import Card from '../card/Card';
+import { Link } from 'react-router-dom';
 
 const CardsContainer = () => {
   const [apartments, setApartments] = useState([]);
@@ -13,11 +14,17 @@ const CardsContainer = () => {
   return (
     <div className='cards__container'>
       {apartments.map((apartment) => (
-        <Card
+        <Link
+          to={`/apartment/${apartment.id}`}
+          className='card__link'
           key={apartment.id}
-          title={apartment.title}
-          image={apartment.cover}
-        />
+        >
+          <Card
+            key={apartment.id}
+            title={apartment.title}
+            image={apartment.cover}
+          />
+        </Link>
       ))}
     </div>
   );
--- src/pages/apartmentPage/ApartmentPage.jsx
+++ src/pages/apartmentPage/ApartmentPage.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ApartmentPage.css';
+import { useParams } from 'react-router-dom';
+import apartmentsList from '../../assets/housingOffers.json';
 
 const ApartmentPage = () => {
+  const { id } = useParams();
+  const [apartment, setApartment] = useState({});
+  useEffect(() => {
+    const selectedApartment = apartmentsList.find(
+      (apartment) => apartment.id === id
+    );
+    setApartment(selectedApartment);
+  }, [id]);
+
   return (
     <div>
-      {' '}
       <p>apartment</p>
     </div>
   );
